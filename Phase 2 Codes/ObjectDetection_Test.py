288c13bd57535473e6c491bd124ba60bfcae7bcc
Miquel H.T. <155618531+miquelht13@users.noreply.github.com>
2025-02-17 15:32:06 +0100

    Update ObjectDetection_Test.py

--- Phase 2 Codes/ObjectDetection_Test.py
+++ Phase 2 Codes/ObjectDetection_Test.py
@@ -1,1 +1,3 @@
 ## CODE HERE ##
+# Python test code for detecting a desired object. In this case, a car.
+# Then, by giving the camera parameters and the known objecto to detect, computes the distance at which the object is from the camera
